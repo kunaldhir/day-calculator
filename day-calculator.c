c3dc617401979062c3aab7de8ab7fb87a04f3cff
Kunal <kunaldhir@live.in>
2014-02-17 23:51:59 +0530

    Create day-calculator.c

---
+++ day-calculator.c
@@ -0,0 +1,19 @@
+#include(math.h)
+#include(conio.h)
+#include(stdio.h)
+
+void main()
+{
+int d,m,yy,D,xx,c;
+float q,a,R;
+char *string;
+printf("\nenter date : ");
+scanf("%d", &d);
+printf("\nenter first two digits of year");
+scanf("%d", &xx);
+printf("\nenter last two digits of year");
+scanf("%d", &yy);
+q=yy/4;
+a=xx*100/400;
+
+
